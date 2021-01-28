8623bc1ef45e1778ed75dd18d499670bdc67585e
Tabea Ratzke <tabratzke@gmail.com>
2021-01-28 08:47:34 +0100

    use API for questions

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,28 +1,31 @@
+import html
+
+
 class QuizBrain:
 
     def __init__(self, question_list):
         self.question_number = 0
         self.score = 0
         self.question_list = question_list
+        self.current_question = None
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
+        question_text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q.{self.question_number}: {question_text} (True/False): ")
+        self.check_answer(user_answer)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             print("You got it right!")
             self.score += 1
         else:
             print("That's wrong.")
+
         print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
-
-    def print_result(self):
-        print("You've completed the quiz")
-        print(f"Your final score was: {self.score}/{self.question_number}")
