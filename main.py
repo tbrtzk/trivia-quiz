155077e534f1935a0778b954a8dd757a7c6ed3ef
Tabea Ratzke <tabratzke@gmail.com>
2021-01-29 12:17:57 +0100

    add UI

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterFace()
+quiz_ui = QuizInterFace(quiz)
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
